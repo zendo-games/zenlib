73dc5a76470fe011d760a7f2a945303c2375a52d
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2023-10-23 00:00:10 -0500

    Add simple test harness

---
+++ src/test/java/zendo/games/zenlib/ZenlibTest.java
@@ -0,0 +1,74 @@
+package zendo.games.zenlib;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import zendo.games.zenlib.screens.ZenScreen;
+
+public class ZenlibTest extends ZenMain {
+
+    public static final ZenConfig config = new ZenConfig();
+
+    public ZenlibTest() {
+        super(config);
+    }
+
+    @Override
+    public ZenAssets createAssets() {
+        return new Assets();
+    }
+
+    @Override
+    public ZenScreen<Assets> createStartScreen() {
+        return new TestScreen();
+    }
+
+    // ------------------------------------------------------------------------
+    // Test Assets (from src/test/resources)
+    // ------------------------------------------------------------------------
+
+    public static class Assets extends ZenAssets {
+        Texture gdx;
+
+        @Override
+        public void loadManagerAssets() {
+            mgr.load("libgdx.png", Texture.class);
+        }
+
+        @Override
+        public void initCachedAssets() {
+            gdx = mgr.get("libgdx.png", Texture.class);
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // Test Screen
+    // ------------------------------------------------------------------------
+
+    public static class TestScreen extends ZenScreen<Assets> {
+        public TestScreen() {
+            super(Assets.class);
+        }
+
+        @Override
+        public void render(SpriteBatch batch) {
+            ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+            batch.setProjectionMatrix(worldCamera.combined);
+            batch.begin();
+            {
+                var image = assets.gdx;
+                var scale = 1 / 4f;
+                var imageWidth = scale * image.getWidth();
+                var imageHeight = scale * image.getHeight();
+                batch.draw(image,
+                        (worldCamera.viewportWidth - imageWidth) / 2f,
+                        (worldCamera.viewportHeight - imageHeight) / 2f,
+                        imageWidth, imageHeight);
+            }
+            batch.end();
+        }
+    }
+
+}
+
