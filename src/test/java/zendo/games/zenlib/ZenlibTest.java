219c16b7b2c22e7798b05f58983df52647a66e88
Jeffrey Hwang <jinsoo.jeffrey.hwang@gmail.com>
2023-10-28 13:35:45 -0500

    test completed

--- src/test/java/zendo/games/zenlib/ZenlibTest.java
+++ src/test/java/zendo/games/zenlib/ZenlibTest.java
@@ -3,7 +3,6 @@ package zendo.games.zenlib;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.kotcrab.vis.ui.widget.VisTextButton;
 import zendo.games.zenlib.assets.ZenPatch;
@@ -55,15 +54,9 @@ public class ZenlibTest extends ZenMain {
         public TestScreen() {
             super(Assets.class);
             Gdx.input.setInputProcessor(uiStage);
-            var button = new VisTextButton("Screen 1");
+            var button = new VisTextButton("Button");
             button.setPosition(100, 100);
             button.getStyle().up = ZenPatch.glass_active.ninePatchDrawable;
-            button.addListener(new ClickListener() {
-                @Override
-                public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-                    ZenlibTest.instance.setScreen(new TestScreen2());
-                }
-            });
             uiStage.addActor(button);
         }
 
@@ -88,48 +81,5 @@ public class ZenlibTest extends ZenMain {
             uiStage.draw();
         }
     }
-
-    // ------------------------------------------------------------------------
-    // Test Screen
-    // ------------------------------------------------------------------------
-
-    public class TestScreen2 extends ZenScreen<Assets> {
-        public TestScreen2() {
-            super(Assets.class);
-            var button = new VisTextButton("Screen 2");
-            Gdx.input.setInputProcessor(uiStage);
-            button.setPosition(100, 100);
-            button.getStyle().up = ZenPatch.glass_red.ninePatchDrawable;
-            button.addListener(new ClickListener() {
-                @Override
-                public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-                    ZenlibTest.instance.setScreen(new TestScreen());
-                }
-            });
-            uiStage.addActor(button);
-        }
-
-
-        @Override
-        public void render(SpriteBatch batch) {
-            ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-
-            batch.setProjectionMatrix(worldCamera.combined);
-            batch.begin();
-            {
-                var image = assets.gdx;
-                var scale = 1 / 2f;
-                var imageWidth = scale * image.getWidth();
-                var imageHeight = scale * image.getHeight();
-                batch.draw(image,
-                        (worldCamera.viewportWidth - imageWidth) / 2f,
-                        (worldCamera.viewportHeight - imageHeight) / 2f,
-                        imageWidth, imageHeight);
-            }
-            batch.end();
-            uiStage.draw();
-        }
-    }
-
 }
 
